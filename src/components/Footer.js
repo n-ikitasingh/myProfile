218d5138db3405b906b1e0a1d2661883d19e138c
n-ikitasingh <nikitasingh.sps@gmail.com>
2025-01-31 13:15:27 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from "react-bootstrap";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+     <Container>
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/nikita-singh-08318924b/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="mailto:nikitasingh.uks@gmail.com"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+
+      </Container>
+
+    </footer>
+  )
+}
